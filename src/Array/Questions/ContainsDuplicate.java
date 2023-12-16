da95c0200585b8698cadcdef70fb3d90d91a95fb
nehakashyap-1204 <kashyapneha080@gmail.com>
2023-12-16 13:33:16 +0530

    added one more element in ContainsDuplicate.java

--- src/Array/Questions/ContainsDuplicate.java
+++ src/Array/Questions/ContainsDuplicate.java
@@ -2,7 +2,7 @@ package Array.Questions;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,5};
+        int[] nums = {1,2,3,5,5};
         System.out.println( duplicate(nums));
     }
     public static boolean duplicate(int[] nums){
