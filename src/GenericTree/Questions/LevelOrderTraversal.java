aff8d53beee6b6f13c292ea9541b87016a2d041b
nehakashyap-1204 <kashyapneha080@gmail.com>
2024-01-02 15:33:05 +0530

    generic tree traversal questions

---
+++ src/GenericTree/Questions/LevelOrderTraversal.java
@@ -0,0 +1,50 @@
+package GenericTree.Questions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public static class Node {
+        int val;
+        List<Node> child;
+        Node(int val) {
+            this.val = val;
+            child = new ArrayList<>();
+        }
+    }
+    public static void formTree(Node root) {
+        root.child.add(new Node(5));
+        root.child.add(new Node(11));
+        root.child.add(new Node(63));
+        root.child.get(0).child.add(new Node(1));
+        root.child.get(0).child.add(new Node(4));
+        root.child.get(0).child.add(new Node(8));
+        root.child.get(1).child.add(new Node(6));
+        root.child.get(1).child.add(new Node(7));
+        root.child.get(1).child.add(new Node(15));
+        root.child.get(2).child.add(new Node(31));
+        root.child.get(2).child.add(new Node(55));
+        root.child.get(2).child.add(new Node(65));
+    }
+    public static void levelOrderTraversal(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node currentNode = q.peek();
+            System.out.print(currentNode.val + " ");
+            q.remove();
+            int noOfChildren = currentNode.child.size();
+            for (int i = 0; i < noOfChildren; i++) {
+                q.add(currentNode.child.get(i));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(30);
+        formTree(root);
+        levelOrderTraversal(root);
+    }
+}
+
---
+++ src/GenericTree/Questions/ZigZagOrderTraversal.java
@@ -0,0 +1,69 @@
+package GenericTree.Questions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import static java.util.Collections.reverse;
+
+public class ZigZagOrderTraversal {
+    public static class Node {
+        int val;
+        List<Node> child;
+        Node(int val) {
+            this.val = val;
+            child = new ArrayList<>();
+        }
+    }
+    public static void formTree(Node root) {
+        root.child.add(new Node(5));
+        root.child.add(new Node(11));
+        root.child.add(new Node(63));
+        root.child.get(0).child.add(new Node(1));
+        root.child.get(0).child.add(new Node(4));
+        root.child.get(0).child.add(new Node(8));
+        root.child.get(1).child.add(new Node(6));
+        root.child.get(1).child.add(new Node(7));
+        root.child.get(1).child.add(new Node(15));
+        root.child.get(2).child.add(new Node(31));
+        root.child.get(2).child.add(new Node(55));
+        root.child.get(2).child.add(new Node(65));
+    }
+    public static void zigzagOrderTraversal(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        boolean flag = false;
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Node> currentLevel = new ArrayList<>();
+            while (levelSize-- > 0) {
+                Node currentNode = q.peek();
+                currentLevel.add(currentNode);
+                q.remove();
+                int noOfChildren = currentNode.child.size();
+                for (int i = 0; i < noOfChildren; i++) {
+                    q.add(currentNode.child.get(i));
+                }
+            }
+            if (flag == false) {
+                printNodeList(currentLevel);
+            } else {
+                reverse(currentLevel);
+                printNodeList(currentLevel);
+            }
+            flag = !flag;
+        }
+    }
+    public static void printNodeList(List<Node> nodeList) {
+        for (int i = 0; i < nodeList.size(); i++) {
+            System.out.print(nodeList.get(i).val + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node root = new Node(30);
+        formTree(root);
+        zigzagOrderTraversal(root);
+    }
+}
